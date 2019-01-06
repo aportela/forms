f70ea96c4b1a4b486c46e08c7ab7d4044d48f9a3
alex <766f6964+github@gmail.com>
2019-01-06 17:59:47 +0100

    added database (sqlite3) support

--- src/AppDependencies.php
+++ src/AppDependencies.php
@@ -24,4 +24,17 @@
         return ($logger);
     };
 
+    $container['databaseLogger'] = function ($c) {
+        $settings = $c->get('settings')['databaseLogger'];
+        $logger = new \Monolog\Logger($settings['name']);
+        $logger->pushProcessor(new \Monolog\Processor\UidProcessor());
+        $handler = new \Monolog\Handler\RotatingFileHandler($settings['path'], 0, $settings['level']);
+        $handler->setFilenameFormat('{date}/{filename}', \Monolog\Handler\RotatingFileHandler::FILE_PER_DAY);
+        $formatter = new \Monolog\Formatter\LineFormatter(null, null, true, true);
+        $handler->setFormatter($formatter);
+        $logger->pushHandler($handler);
+        return ($logger);
+    };
+
+
 ?>
--- src/AppSettings.php
+++ src/AppSettings.php
@@ -6,10 +6,25 @@
 
     return [
         'settings' => [
+            'phpRequiredExtensions' => array('pdo_sqlite', 'mbstring'),
+            // database settings
+            'database' => [
+                'type' => "PDO_SQLITE", // supported types: PDO_SQLITE
+                'name' => 'forms',
+                'username' => '',
+                'password' => '',
+                'host' => '',
+                'port' => 0,
+            ],
             // Monolog settings
             'logger' => [
                 'name' => 'forms-app',
                 'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/default.log',
+                'level' => \Monolog\Logger::DEBUG
+            ],
+            'databaseLogger' => [
+                'name' => 'forms-db',
+                'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/database.log',
                 'level' => \Monolog\Logger::DEBUG
             ],
         ],
