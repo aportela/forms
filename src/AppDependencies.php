ba2708e322de9598755a4349cb3701a2703d5344
alex <766f6964+github@gmail.com>
2019-01-06 17:39:54 +0100

    hack for enable php-slim routes in a subfolder

--- src/AppDependencies.php
+++ src/AppDependencies.php
@@ -6,4 +6,12 @@
 
     $container = $this->app->getContainer();
 
+    // enable routes in a subfolder
+    // https://github.com/slimphp/Slim/issues/2294#issuecomment-341887867
+    $container['environment'] = function () {
+        $scriptName = $_SERVER['SCRIPT_NAME'];
+        $_SERVER['SCRIPT_NAME'] = dirname(dirname($scriptName)) . '/' . basename($scriptName);
+        return new \Slim\Http\Environment($_SERVER);
+    };
+
 ?>
