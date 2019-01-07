76d45c4f9daee644ba32c7dbe7fbcd7fa2cff9c1
alex <766f6964+github@gmail.com>
2019-01-07 19:54:22 +0100

    added minimal api routes and tests

--- src/AppRoutes.php
+++ src/AppRoutes.php
@@ -16,9 +16,27 @@
                 array(
                     'upgradeAvailable' => $v->hasUpgradeAvailable(),
                     'logged' => \Forms\UserSession::isLogged(),
-                    'allowSignUp' => $this->get('settings')['common']['allowSignUp']
+                    'allowSignUp' => $this->get('settings')['common']['allowSignUp'],
+                    'sessionTimeout' => ini_get("session.gc_maxlifetime")
                 )
             )
         ));
     });
+
+    $this->app->group("/api", function() {
+        $this->get('/poll', function (Request $request, Response $response, array $args) {
+            $this->logger->info($request->getOriginalMethod() . " " . $request->getUri()->getPath());
+            $v = new \Forms\Database\Version(new \Forms\Database\DB($this), $this->get('settings')['database']['type']);
+            return $response->withJson([
+                'initialState' =>
+                    array(
+                        'upgradeAvailable' => $v->hasUpgradeAvailable(),
+                        'logged' => \Forms\UserSession::isLogged(),
+                        'allowSignUp' => $this->get('settings')['common']['allowSignUp'],
+                        'sessionTimeout' => ini_get("session.gc_maxlifetime")
+                    )
+            ], 200);
+        });
+
+    })->add(new \Forms\Middleware\APIExceptionCatcher($this->app->getContainer()));
 ?>
