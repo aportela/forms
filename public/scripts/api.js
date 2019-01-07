4ac7f2839c4183b89fc1f247f6bbad8b9112e32b
alex <766f6964+github@gmail.com>
2019-01-07 20:06:37 +0100

    new user api routes

---
+++ public/scripts/api.js
@@ -0,0 +1,60 @@
+"use strict";
+
+/**
+ * common object for interact with API
+ * all methods return callback with vue-resource response object
+ */
+const formsAPI = {
+    user: {
+        signUp: function (email, password, callback) {
+            var params = {
+                email: email,
+                password: password
+            }
+            Vue.http.post("api/user/signup", params).then(
+                response => {
+                    if (callback && typeof callback === "function") {
+                        callback(response);
+                    }
+                },
+                response => {
+                    if (callback && typeof callback === "function") {
+                        callback(response);
+                    }
+                }
+            );
+        },
+        signIn: function (email, password, callback) {
+            var params = {
+                email: email,
+                password: password
+            }
+            Vue.http.post("api/user/signin", params).then(
+                response => {
+                    if (callback && typeof callback === "function") {
+                        callback(response);
+                    }
+                },
+                response => {
+                    if (callback && typeof callback === "function") {
+                        callback(response);
+                    }
+                }
+            );
+        },
+        signOut: function (callback) {
+            Vue.http.get("api/user/signout").then(
+                response => {
+                    if (callback && typeof callback === "function") {
+                        callback(response);
+                    }
+                },
+                response => {
+                    if (callback && typeof callback === "function") {
+                        callback(response);
+                    }
+                }
+            );
+        }
+    }
+};
--- src/AppRoutes.php
+++ src/AppRoutes.php
@@ -38,5 +38,53 @@
             ], 200);
         });
 
+        /**
+         * USER API routes (BEGIN)
+         */
+        $this->group("/user", function() {
+            $this->post('/signin', function (Request $request, Response $response, array $args) {
+                $u = new \Forms\User("", $request->getParam("email", ""), $request->getParam("password", ""));
+                if ($u->login(new \Forms\Database\DB($this))) {
+                    return $response->withJson(['logged' => true], 200);
+                } else {
+                    return $response->withJson(['logged' => false], 401);
+                }
+            });
+
+            $this->post('/signup', function (Request $request, Response $response, array $args) {
+                if ($this->get('settings')['common']['allowSignUp']) {
+                    $dbh = new \Forms\Database\DB($this);
+                    $u = new \Forms\User(
+                        "",
+                        $request->getParam("email", ""),
+                        $request->getParam("password", "")
+                    );
+                    $exists = false;
+                    try {
+                        $u->get($dbh);
+                        $exists = true;
+                    } catch (\Forms\Exception\NotFoundException $e) {
+                    }
+                    if ($exists) {
+                        return $response->withJson([], 409);
+                    } else {
+                        $u->id = (\Ramsey\Uuid\Uuid::uuid4())->toString();
+                        $u->add($dbh);
+                        return $response->withJson([], 200);
+                    }
+                } else {
+                    throw new \Forms\Exception\AccessDeniedException("");
+                }
+            });
+
+            $this->get('/signout', function (Request $request, Response $response, array $args) {
+                \Forms\User::logout();
+                return $response->withJson(['logged' => false], 200);
+            });
+        });
+        /**
+         * USER API routes (END)
+         */
+
     })->add(new \Forms\Middleware\APIExceptionCatcher($this->app->getContainer()));
 ?>
