c411e9e5fae0180b7b6474a93b46691b0b39c0e7
alex <766f6964+github@gmail.com>
2019-01-10 22:55:27 +0100

    new class for manage search results

---
+++ src/SearchResult.php
@@ -0,0 +1,36 @@
+<?php
+
+    declare(strict_types=1);
+
+    namespace Forms;
+    class SearchResult {
+
+        public $currentPage;
+        public $resultsPage;
+        public $totalResults;
+        public $totalPages;
+
+        public function __construct (int $currentPage = 1, int $resultsPage = 0, int $totalResults = 0) {
+            $this->currentPage = $currentPage;
+            $this->resultsPage = $resultsPage;
+            $this->totalResults = $totalResults;
+            if ($resultsPage > 0) {
+                $this->totalPages = ceil($this->totalResults / $resultsPage);
+            } else {
+                $this->totalPages = $this->totalResults > 0 ? 1: 0;
+            }
+
+        }
+
+        public function __destruct() { }
+
+        public function getSQLPageOffset() {
+            return($this->resultsPage * ($this->currentPage - 1));
+        }
+
+        public function isPaginationEnabled() {
+            return($this->resultsPage != 0);
+        }
+
+    }
+?>
