f5ae7374a32f0104d31f281e6c36fa2d0e230169
alex <766f6964+github@gmail.com>
2019-01-12 19:40:58 +0100

    updated breadcrumb admin links

--- public/scripts/components/section-breadcrumb.js
+++ public/scripts/components/section-breadcrumb.js
@@ -5,9 +5,9 @@ const vueFormsSectionBreadCrumb = (function () {
         return `
             <nav class="breadcrumb has-bullet-separator" aria-label="breadcrumbs">
                 <ul>
-                <li><a href="#"><span class="icon is-small"><i class="fas fa-home" aria-hidden="true"></i></span><span>Home</span></a></li>
-                <li><a href="#"><span class="icon is-small"><i class="fas fa-tools" aria-hidden="true"></i></span><span>Administration</span></a></li>
-                <li class="is-active" aria-current="page"><a href="#"><span class="icon is-small"><i class="fas fa-user" aria-hidden="true"></i></span><span>User management</span></a></li>
+                <li><a href="#"><span class="icon is-small" v-on:click.prevent="changeRoute('home')"><i class="fas fa-home" aria-hidden="true"></i></span><span>Home</span></a></li>
+                <li v-if="isAdminRoute"><a href="#"><span class="icon is-small"><i class="fas fa-tools" aria-hidden="true"></i></span><span>Administration</span></a></li>
+                <li v-if="isRouteActive('users')" class="is-active" aria-current="page"><a href="#"><span class="icon is-small"><i class="fas fa-user" aria-hidden="true"></i></span><span>User management</span></a></li>
                 </ul>
             </nav>
         `;
@@ -18,6 +18,19 @@ const vueFormsSectionBreadCrumb = (function () {
         data: function () {
             return ({
             });
+        },
+        mixins: [
+            mixinRoutes
+        ],
+        computed: {
+            isAdminRoute: function() {
+                return(
+                    this.isRouteActive("users") ||
+                    this.isRouteActive("userCard") ||
+                    this.isRouteActive("groups") ||
+                    this.isRouteActive("attributes")
+                );
+            }
         }
     });
 
