31ff30f583e37008cc680484bb26ebdeaf5250d5
alex <766f6964+github@gmail.com>
2019-01-13 00:24:01 +0100

    added group manage sections

--- public/scripts/components/section-breadcrumb.js
+++ public/scripts/components/section-breadcrumb.js
@@ -6,11 +6,12 @@ const vueFormsSectionBreadCrumb = (function () {
             <nav class="breadcrumb has-bullet-separator" aria-label="breadcrumbs">
                 <ul>
                 <li><a href="#" v-on:click.prevent="changeRoute('home')"><span class="icon is-small"><i class="fas fa-home" aria-hidden="true"></i></span><span>Home</span></a></li>
-                <li v-if="isRouteActive('profile')" aria-current="page"><a href="#"><span class="icon is-small"><i class="fas fa-user-cog" aria-hidden="true"></i></span><span>Profile</span></a></li>
+                <li v-if="isRouteActive('profile')" aria-current="page"><a href="#"><span class="icon is-small"><i class="fas fa-user-cog" aria-hidden="true"></i></span><span>My profile</span></a></li>
                 <li v-if="isAdminRoute"><a href="#"><span class="icon is-small"><i class="fas fa-tools" aria-hidden="true"></i></span><span>Administration</span></a></li>
                 <li v-if="isRouteActive('users') || isRouteActive('addUser') || isRouteActive('updateUser')" aria-current="page"><a href="#" v-on:click.prevent="changeRoute('users')"><span class="icon is-small"><i class="fas fa-user" aria-hidden="true"></i></span><span>User management</span></a></li>
                 <li v-if="isRouteActive('addUser')" aria-current="page"><a href="#"><span class="icon is-small"><i class="fas fa-user-plus" aria-hidden="true"></i></span><span>Add user</span></a></li>
-                <li v-if="isRouteActive('updateUser')" aria-current="page"><a href="#"><span class="icon is-small"><i class="fas fa-user-cog" aria-hidden="true"></i></span><span>Update user</span></a></li>
+                <li v-if="isRouteActive('updateUser')" aria-current="page"><a href="#"><span class="icon is-small"><i class="fas fa-pen" aria-hidden="true"></i></span><span>Update user</span></a></li>
+                <li v-if="isRouteActive('groups') || isRouteActive('addGroup') || isRouteActive('updateGroup')" aria-current="page"><a href="#" v-on:click.prevent="changeRoute('groups')"><span class="icon is-small"><i class="fas fa-users" aria-hidden="true"></i></span><span>Group management</span></a></li>
                 </ul>
             </nav>
         `;
