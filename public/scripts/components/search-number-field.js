3b6e1a1107fdc4de85e42fb4b124d7950e70bf5a
alex <766f6964+github@gmail.com>
2019-01-13 00:21:24 +0100

    new custom vue component for filtering numbers

---
+++ public/scripts/components/search-number-field.js
@@ -0,0 +1,45 @@
+const vueFormsSearchNumberField = (function () {
+    "use strict";
+
+    let template = function () {
+        return `
+            <div class="field has-addons is-one-fifth">
+                <div class="control has-icons-left is-expanded">
+                    <input class="input" min="0" type="number" v-bind:placeholder="placeholderfrom" v-bind:disabled="disabled" v-model.number.trim="range.from" v-on:keyup.enter.prevent="$emit('searchTriggered', range)">
+                    <span  class="icon is-small is-left">
+                        <i class="fas fa-filter"></i>
+                    </span >
+                </div>
+                <div class="control is-expanded">
+                    <input class="input" min="0" type="number" v-bind:placeholder="placeholderto" v-bind:disabled="disabled" v-model.number.trim="range.to" v-on:keyup.enter.prevent="$emit('searchTriggered', range)">
+                </div>
+                <div class="control">
+                    <a class="button" v-bind:disabled="disabled" v-on:click.prevent="$emit('searchTriggered', range)">
+                        <span class="icon">
+                            <i class="fas fa-search"></i>
+                        </span>
+                    </a>
+                </div>
+            </div>
+        `;
+    };
+
+    let module = Vue.component('f-search-number-field', {
+        template: template(),
+        data: function () {
+            return ({
+                range: {
+                    from: null,
+                    to: null
+                }
+            });
+        },
+        props: [
+            'disabled',
+            'placeholderfrom',
+            'placeholderto'
+        ]
+    });
+
+    return (module);
+})();
