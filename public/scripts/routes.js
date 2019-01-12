9a6d11e0014e9fbf79a1074f69d2e900d41e2412
alex <766f6964+github@gmail.com>
2019-01-12 19:27:45 +0100

    moved client admin routes paths

--- public/scripts/routes.js
+++ public/scripts/routes.js
@@ -7,10 +7,11 @@ const routes = [
         path: '/app',
         component: vueFormsSectionContainer,
         children: [
-            { path: '/home', name: 'home', component: vueFormsHome },
-            { path: '/users', name: 'users', component: vueFormsUsers },
-            { path: '/groups', name: 'groups', component: vueFormsGroups },
-            { path: '/attributes', name: 'attributes', component: vueFormsAttributes }
+            { path: 'home', name: 'home', component: vueFormsHome },
+            { path: 'administration/users', name: 'users', component: vueFormsUsers },
+            { path: 'administration/users/:id', name: 'userCard', component: vueFormsUserCard },
+            { path: 'administration/groups', name: 'groups', component: vueFormsGroups },
+            { path: 'administration/attributes', name: 'attributes', component: vueFormsAttributes }
         ]
     },
     { path: '/api-error', name: 'apiError', component: vueFormsSectionAPIError }
