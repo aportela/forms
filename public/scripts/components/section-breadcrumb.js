b57a7234bae4ebb4eb608d0311d0978f49ee3b2f
alex <766f6964+github@gmail.com>
2019-01-13 00:40:22 +0100

    new sections for group management

--- public/scripts/components/section-breadcrumb.js
+++ public/scripts/components/section-breadcrumb.js
@@ -9,9 +9,11 @@ const vueFormsSectionBreadCrumb = (function () {
                 <li v-if="isRouteActive('profile')" aria-current="page"><a href="#"><span class="icon is-small"><i class="fas fa-user-cog" aria-hidden="true"></i></span><span>My profile</span></a></li>
                 <li v-if="isAdminRoute"><a href="#"><span class="icon is-small"><i class="fas fa-tools" aria-hidden="true"></i></span><span>Administration</span></a></li>
                 <li v-if="isRouteActive('users') || isRouteActive('addUser') || isRouteActive('updateUser')" aria-current="page"><a href="#" v-on:click.prevent="changeRoute('users')"><span class="icon is-small"><i class="fas fa-user" aria-hidden="true"></i></span><span>User management</span></a></li>
-                <li v-if="isRouteActive('addUser')" aria-current="page"><a href="#"><span class="icon is-small"><i class="fas fa-user-plus" aria-hidden="true"></i></span><span>Add user</span></a></li>
+                <li v-if="isRouteActive('addUser')" aria-current="page"><a href="#"><span class="icon is-small"><i class="fas fa-plus" aria-hidden="true"></i></span><span>Add user</span></a></li>
                 <li v-if="isRouteActive('updateUser')" aria-current="page"><a href="#"><span class="icon is-small"><i class="fas fa-pen" aria-hidden="true"></i></span><span>Update user</span></a></li>
                 <li v-if="isRouteActive('groups') || isRouteActive('addGroup') || isRouteActive('updateGroup')" aria-current="page"><a href="#" v-on:click.prevent="changeRoute('groups')"><span class="icon is-small"><i class="fas fa-users" aria-hidden="true"></i></span><span>Group management</span></a></li>
+                <li v-if="isRouteActive('addGroup')" aria-current="page"><a href="#"><span class="icon is-small"><i class="fas fa-plus" aria-hidden="true"></i></span><span>Add group</span></a></li>
+                <li v-if="isRouteActive('updateGroup')" aria-current="page"><a href="#"><span class="icon is-small"><i class="fas fa-pen" aria-hidden="true"></i></span><span>Update group</span></a></li>
                 </ul>
             </nav>
         `;
@@ -33,6 +35,8 @@ const vueFormsSectionBreadCrumb = (function () {
                     this.isRouteActive("addUser") ||
                     this.isRouteActive("updateUser") ||
                     this.isRouteActive("groups") ||
+                    this.isRouteActive("addGroup") ||
+                    this.isRouteActive("updateGroup") ||
                     this.isRouteActive("attributes")
                 );
             }
