ae4c6d955b6f8823ec05a38b2b37e4098e64c171
alex <766f6964+github@gmail.com>
2019-01-12 20:40:28 +0100

    new client routes

--- public/scripts/routes.js
+++ public/scripts/routes.js
@@ -8,8 +8,10 @@ const routes = [
         component: vueFormsSectionContainer,
         children: [
             { path: 'home', name: 'home', component: vueFormsHome },
-            { path: 'administration/users', name: 'users', component: vueFormsUsers },
-            { path: 'administration/users/:id', name: 'userCard', component: vueFormsUserCard },
+            { path: 'administration/users/search', name: 'users', component: vueFormsUsers },
+            { path: 'administration/users/add', name: 'addUser', component: vueFormsUserCard },
+            { path: 'administration/users/update/:id', name: 'updateUser', component: vueFormsUserCard },
+            { path: 'profile', name: 'profile', component: vueFormsUserCard },
             { path: 'administration/groups', name: 'groups', component: vueFormsGroups },
             { path: 'administration/attributes', name: 'attributes', component: vueFormsAttributes }
         ]
