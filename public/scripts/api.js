09b06c6d7db1dd62ad2b8e113dcb44116a0a9f72
alex <766f6964+github@gmail.com>
2019-01-12 10:16:07 +0100

    added user search method

--- public/scripts/api.js
+++ public/scripts/api.js
@@ -7,7 +7,7 @@
 const formsAPI = {
     user: {
         signUp: function (email, name, password, callback) {
-            var params = {
+            let params = {
                 email: email,
                 name: name,
                 password: password
@@ -26,7 +26,7 @@ const formsAPI = {
             );
         },
         signIn: function (email, password, callback) {
-            var params = {
+            let params = {
                 email: email,
                 password: password
             }
@@ -45,6 +45,42 @@ const formsAPI = {
         },
         signOut: function (callback) {
             Vue.http.get("api/user/signout").then(
+                response => {
+                    if (callback && typeof callback === "function") {
+                        callback(response);
+                    }
+                },
+                response => {
+                    if (callback && typeof callback === "function") {
+                        callback(response);
+                    }
+                }
+            );
+        },
+        search: function(searchByEmail, searchByName, searchByAccountType, searchFromCreationDate, searchToCreationDate, currentPage, resultsPage, sortBy, sortOrder, callback) {
+            let params = {
+                filter: {},
+                currentPage: currentPage,
+                resultsPage: resultsPage,
+                sortBy: sortBy,
+                sortOrder: sortOrder,
+            };
+            if (searchByEmail) {
+                params.filter.email = searchByEmail;
+            }
+            if (searchByName) {
+                params.filter.name = searchByName;
+            }
+            if (searchByAccountType) {
+                params.filter.accountType = searchByAccountType;
+            }
+            if (searchFromCreationDate) {
+                params.filter.fromCreationDate = searchFromCreationDate;
+            }
+            if (searchToCreationDate) {
+                params.filter.toCreationDate = searchToCreationDate;
+            }
+            Vue.http.post("api/user/search", params).then(
                 response => {
                     if (callback && typeof callback === "function") {
                         callback(response);
