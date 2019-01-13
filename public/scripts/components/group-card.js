abc96405d6322106529e69242f66e4d850764e06
alex <766f6964+github@gmail.com>
2019-01-13 19:11:44 +0100

    added group user list

--- public/scripts/components/group-card.js
+++ public/scripts/components/group-card.js
@@ -7,19 +7,42 @@ const vueFormsGroupCard = (function () {
                 <form v-on:submit.prevent="save()">
                     <div class="box">
                         <label class="label">Name</label>
-                        <p class="control has-icons-left" id="login-container" v-bind:class="{ 'has-icons-right' : validator.hasInvalidField('name') }">
+                        <p class="control has-icons-left" v-bind:class="{ 'has-icons-right' : validator.hasInvalidField('name') }">
                             <input class="input" type="text" maxlength="255" required v-bind:class="{ 'is-danger': validator.hasInvalidField('name') }" v-bind:disabled="loading ? true: false" v-model="group.name">
-                            <span class="icon is-small is-left"><i class="fa fa-envelope"></i></span>
+                            <span class="icon is-small is-left"><i class="fa fa-users"></i></span>
                             <span class="icon is-small is-right" v-show="validator.hasInvalidField('name')"><i class="fa fa-warning"></i></span>
                             <p class="help is-danger" v-show="validator.hasInvalidField('name')">{{ validator.getInvalidFieldMessage('name') }}</p>
                         </p>
                         <label class="label">Description</label>
-                        <p class="control has-icons-left" id="login-container" v-bind:class="{ 'has-icons-right' : validator.hasInvalidField('description') }">
+                        <p class="control has-icons-left" v-bind:class="{ 'has-icons-right' : validator.hasInvalidField('description') }">
                             <input class="input" type="text" maxlength="255" v-bind:class="{ 'is-danger': validator.hasInvalidField('description') }" v-bind:disabled="loading ? true: false" v-model="group.description">
-                            <span class="icon is-small is-left"><i class="fa fa-envelope"></i></span>
+                            <span class="icon is-small is-left"><i class="fa fa-info"></i></span>
                             <span class="icon is-small is-right" v-show="validator.hasInvalidField('description')"><i class="fa fa-warning"></i></span>
                             <p class="help is-danger" v-show="validator.hasInvalidField('description')">{{ validator.getInvalidFieldMessage('description') }}</p>
                         </p>
+                        <table class="table is-striped is-narrow is-fullwidth is-unselectable">
+                            <thead>
+                                <tr>
+                                    <th>Email</th>
+                                    <th>Name</th>
+                                    <th class="has-text-centered">Operations</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="user in group.users" v-bind:key="user.id">
+                                    <td>{{ user.email }}</td>
+                                    <td>{{ user.name }}</td>
+                                    <td>
+                                        <p class="control has-text-centered">
+                                            <a class="button is-small is-danger" v-bind:disabled="loading" v-on:click.prevent="removeUser(user.id)">
+                                                <span class="icon is-small"><i class="fas fa-trash-alt"></i></span>
+                                                <span>Remove</span>
+                                            </a>
+                                        </p>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
                         <hr>
                         <p class="control">
                             <button type="submit" class="button is-primary" v-bind:class="{ 'is-loading': loading }" v-bind:disabled="loading ? true: false">
@@ -44,7 +67,7 @@ const vueFormsGroupCard = (function () {
                     id: null,
                     name: null,
                     description: null,
-                    users: []
+                    users: [ ]
                 }
             });
         },
@@ -62,6 +85,9 @@ const vueFormsGroupCard = (function () {
             }
         },
         methods: {
+            removeUser: function(userId) {
+                this.group.users = this.group.users.filter(user => user.id !== userId);
+            },
             load: function (id) {
                 let self = this;
                 self.loading = true;
