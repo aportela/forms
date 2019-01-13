be302819cff22e5ebfa91b06ccad58452034661b
alex <766f6964+github@gmail.com>
2019-01-13 17:03:03 +0100

    updates to support validator module

--- public/scripts/vue-auth.js
+++ public/scripts/vue-auth.js
@@ -32,18 +32,18 @@ var vueFormsAuth = (function () {
                         <form v-on:submit.prevent="submitSignIn" v-if="tab == 'signin'">
                             <div class="box">
                                 <label class="label">Email</label>
-                                <p class="control has-icons-left" id="login-container" v-bind:class="{ 'has-icons-right' : invalidSignInEmail }">
-                                    <input class="input" type="email" name="email" maxlength="255" ref="signInEmail" required autofocus v-bind:class="{ 'is-danger': invalidSignInEmail }" v-bind:disabled="loading ? true: false" v-model="signInEmail">
+                                <p class="control has-icons-left" id="login-container" v-bind:class="{ 'has-icons-right' : validator.hasInvalidField('email') }">
+                                    <input class="input" type="email" name="email" maxlength="255" ref="signInEmail" required autofocus v-bind:class="{ 'is-danger': validator.hasInvalidField('email') }" v-bind:disabled="loading ? true: false" v-model="signInEmail">
                                     <span class="icon is-small is-left"><i class="fa fa-envelope"></i></span>
-                                    <span class="icon is-small is-right" v-show="invalidSignInEmail"><i class="fa fa-warning"></i></span>
-                                    <p class="help is-danger" v-show="invalidSignInEmail">Invalid email</p>
+                                    <span class="icon is-small is-right" v-show="validator.hasInvalidField('email')"><i class="fa fa-warning"></i></span>
+                                    <p class="help is-danger" v-show="validator.hasInvalidField('email')">{{ validator.getInvalidFieldMessage('email') }}</p>
                                 </p>
                                 <label class="label">Password</label>
-                                <p class="control has-icons-left" id="password-container" v-bind:class="{ 'has-icons-right' : invalidSignInPassword }">
-                                    <input class="input" type="password" name="password" required v-bind:class="{ 'is-danger': invalidSignInPassword }" v-bind:disabled="loading ? true: false" v-model="signInPassword">
+                                <p class="control has-icons-left" id="password-container" v-bind:class="{ 'has-icons-right' : validator.hasInvalidField('password') }">
+                                    <input class="input" type="password" name="password" required v-bind:class="{ 'is-danger': validator.hasInvalidField('password') }" v-bind:disabled="loading ? true: false" v-model="signInPassword">
                                     <span class="icon is-small is-left"><i class="fa fa-key"></i></span>
-                                    <span class="icon is-small is-right" v-show="invalidSignInPassword"><i class="fa fa-warning"></i></span>
-                                    <p class="help is-danger" v-show="invalidSignInPassword">Invalid password</p>
+                                    <span class="icon is-small is-right" v-show="validator.hasInvalidField('password')"><i class="fa fa-warning"></i></span>
+                                    <p class="help is-danger" v-show="validator.hasInvalidField('password')">{{ validator.getInvalidFieldMessage('password') }}</p>
                                 </p>
                                 <hr>
                                 <p class="control">
@@ -57,27 +57,25 @@ var vueFormsAuth = (function () {
                         <form v-on:submit.prevent="submitSignUp" v-if="tab == 'signup'">
                             <div class="box">
                                 <label class="label">Email</label>
-                                <p class="control has-icons-left" id="login-container" v-bind:class="{ 'has-icons-right' : invalidSignUpEmailFormat || invalidSignUpEmailAlreadyExists }">
-                                    <input class="input" type="email" name="email" maxlength="255" required autofocus v-bind:class="{ 'is-danger': invalidSignUpEmailFormat || invalidSignUpEmailAlreadyExists }" v-bind:disabled="loading ? true: false" v-model="signUpEmail">
+                                <p class="control has-icons-left" id="login-container" v-bind:class="{ 'has-icons-right' : validator.hasInvalidField('email') }">
+                                    <input class="input" type="email" name="email" maxlength="255" required autofocus v-bind:class="{ 'is-danger': validator.hasInvalidField('email') }" v-bind:disabled="loading ? true: false" v-model="signUpEmail">
                                     <span class="icon is-small is-left"><i class="fa fa-envelope"></i></span>
-                                    <span class="icon is-small is-right" v-show="invalidSignUpEmailFormat || invalidSignUpEmailAlreadyExists"><i class="fa fa-warning"></i></span>
-                                    <p class="help is-danger" v-show="invalidSignUpEmailFormat">Invalid email</p>
-                                    <p class="help is-danger" v-show="invalidSignUpEmailAlreadyExists">Email already used</p>
+                                    <span class="icon is-small is-right" v-show="validator.hasInvalidField('email')"><i class="fa fa-warning"></i></span>
+                                    <p class="help is-danger" v-show="validator.hasInvalidField('email')">{{ validator.getInvalidFieldMessage('email') }}</p>
                                 </p>
                                 <label class="label">Name</label>
-                                <p class="control has-icons-left" id="name-container" v-bind:class="{ 'has-icons-right' : invalidSignUpNameFormat || invalidSignUpNameAlreadyExists }">
-                                    <input class="input" type="test" name="name" maxlength="255" required v-bind:class="{ 'is-danger': invalidSignUpNameFormat || invalidSignUpNameAlreadyExists }" v-bind:disabled="loading ? true: false" v-model="signUpName">
+                                <p class="control has-icons-left" id="name-container" v-bind:class="{ 'has-icons-right' : validator.hasInvalidField('name') }">
+                                    <input class="input" type="test" name="name" maxlength="255" required v-bind:class="{ 'is-danger': validator.hasInvalidField('name') }" v-bind:disabled="loading ? true: false" v-model="signUpName">
                                     <span class="icon is-small is-left"><i class="fa fa-envelope"></i></span>
-                                    <span class="icon is-small is-right" v-show="invalidSignUpNameFormat || invalidSignUpNameAlreadyExists"><i class="fa fa-warning"></i></span>
-                                    <p class="help is-danger" v-show="invalidSignUpNameFormat">Invalid name</p>
-                                    <p class="help is-danger" v-show="invalidSignUpNameAlreadyExists">Name already used</p>
+                                    <span class="icon is-small is-right" v-show="validator.hasInvalidField('name')"><i class="fa fa-warning"></i></span>
+                                    <p class="help is-danger" v-show="validator.hasInvalidField('name')">{{ validator.getInvalidFieldMessage('name') }}</p>
                                 </p>
                                 <label class="label">Password</label>
-                                <p class="control has-icons-left" id="password-container" v-bind:class="{ 'has-icons-right' : invalidSignUpPassword }">
-                                    <input class="input" type="password" name="password" required v-bind:class="{ 'is-danger': invalidSignUpPassword }" v-bind:disabled="loading ? true: false" v-model="signUpPassword">
+                                <p class="control has-icons-left" id="password-container" v-bind:class="{ 'has-icons-right' : validator.hasInvalidField('password') }">
+                                    <input class="input" type="password" name="password" required v-bind:class="{ 'is-danger': validator.hasInvalidField('password') }" v-bind:disabled="loading ? true: false" v-model="signUpPassword">
                                     <span class="icon is-small is-left"><i class="fa fa-key"></i></span>
-                                    <span class="icon is-small is-right" v-show="invalidSignUpPassword"><i class="fa fa-warning"></i></span>
-                                    <p class="help is-danger" v-show="invalidSignUpPassword">Invalid password</p>
+                                    <span class="icon is-small is-right" v-show="validator.hasInvalidField('password')"><i class="fa fa-warning"></i></span>
+                                    <p class="help is-danger" v-show="validator.hasInvalidField('password')">{{ validator.getInvalidFieldMessage('password') }}</p>
                                 </p>
                                 <hr>
                                 <p class="control">
@@ -106,20 +104,12 @@ var vueFormsAuth = (function () {
         data: function () {
             return ({
                 loading: false,
+                validator: getValidator(),
                 signInEmail: null,
                 signInPassword: null,
-                invalidSignInEmail: false,
-                invalidSignInName: false,
-                invalidSignInPassword: false,
                 signUpEmail: null,
                 signUpName: null,
                 signUpPassword: null,
-                invalidSignUpEmailFormat: false,
-                invalidSignUpEmailAlreadyExists: false,
-                invalidSignUpNameFormat: false,
-                invalidSignUpNameAlreadyExists: false,
-                invalidSignUpPassword: false,
-                errors: false,
                 tab: 'signin'
             });
         },
@@ -137,8 +127,7 @@ var vueFormsAuth = (function () {
         methods: {
             submitSignIn: function () {
                 var self = this;
-                self.invalidSignInEmail = false;
-                self.invalidSignInPassword = false;
+                self.validator.clear();
                 self.loading = true;
                 formsAPI.user.signIn(this.signInEmail, this.signInPassword, function (response) {
                     if (response.ok) {
@@ -148,21 +137,21 @@ var vueFormsAuth = (function () {
                         switch (response.status) {
                             case 400:
                                 if (response.body.invalidOrMissingParams.find(function (e) { return (e === "email"); })) {
-                                    self.invalidSignInEmail = true;
+                                    self.validator.setInvalid("email", "Invalid email");
                                 } else if (response.body.invalidOrMissingParams.find(function (e) { return (e === "password"); })) {
-                                    self.invalidSignInPassword = true;
+                                    self.validator.setInvalid("email", "Invalid password");
                                 } else {
                                     self.showApiError(response.getApiErrorData());
                                 }
                                 break;
                             case 404:
-                                self.invalidSignInEmail = true;
+                                self.validator.setInvalid("email", "Email not found");
                                 break;
                             case 401:
-                                self.invalidSignInPassword = true;
+                                self.validator.setInvalid("password", "Incorrect password");
                                 break;
                             default:
-                                self.showApiError(response.getApiErrorData());
+                            self.showApiError(response.getApiErrorData());
                                 break;
                         }
                         self.loading = false;
@@ -171,11 +160,7 @@ var vueFormsAuth = (function () {
             },
             submitSignUp: function () {
                 var self = this;
-                self.invalidSignUpEmailFormat = false;
-                self.invalidSignUpEmailAlreadyExists = false;
-                self.invalidSignUpNameFormat = false;
-                self.invalidSignUpNameAlreadyExists = false;
-                self.invalidSignUpPassword = false;
+                self.validator.clear();
                 self.loading = true;
                 formsAPI.user.signUp(this.signUpEmail, this.signUpName, this.signUpPassword, function (response) {
                     if (response.ok) {
@@ -188,24 +173,26 @@ var vueFormsAuth = (function () {
                         switch (response.status) {
                             case 400:
                                 if (response.body.invalidOrMissingParams.find(function (e) { return (e === "email"); })) {
-                                    self.invalidSignUpEmailFormat = true;
+                                    self.validator.setInvalid("email", "Invalid email");
                                 } else if (response.body.invalidOrMissingParams.find(function (e) { return (e === "name"); })) {
-                                    self.invalidSignUpNameFormat = true;
+                                    self.validator.setInvalid("name", "Invalid name");
                                 } else if (response.body.invalidOrMissingParams.find(function (e) { return (e === "password"); })) {
-                                    self.invalidSignUpPassword = true;
+                                    self.validator.setInvalid("email", "Invalid password");
                                 } else {
-                                    self.apiError = response.getApiErrorData();
+                                    self.showApiError(response.getApiErrorData());
                                 }
                                 break;
                             case 409:
                                 if (response.body.invalidParams.find(function (e) { return (e === "email"); })) {
-                                    self.invalidSignUpEmailAlreadyExists = true;
+                                    self.validator.setInvalid("email", "Email already used");
                                 } else if (response.body.invalidParams.find(function (e) { return (e === "name"); })) {
-                                    self.invalidSignUpNameAlreadyExists = true;
+                                    self.validator.setInvalid("name", "Name already used");
+                                } else {
+                                    self.showApiError(response.getApiErrorData());
                                 }
                                 break;
                             default:
-                                self.setAPIError(response.getApiErrorData());
+                            self.showApiError(response.getApiErrorData());
                                 break;
                         }
                         self.loading = false;
