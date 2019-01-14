2ecb0c63ecf4d917484e8b3180c201f1c6d73ab9
alex <766f6964+github@gmail.com>
2019-01-14 19:16:25 +0100

    replaced uuid generator

--- public/scripts/mixins.js
+++ public/scripts/mixins.js
@@ -95,17 +95,12 @@ const mixinTableControls = {
 const mixinUtils = {
     methods: {
         /**
-         * uuid generator
+         * uuid v4 generator (require ES6 crypto api)
          *
-         * (broofa) http://stackoverflow.com/a/2117523
+         * (broofa) https://stackoverflow.com/a/2117523
          */
         uuid: function () {
-            var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-                var r = Math.random() * 16 | 0,
-                    v = c == 'x' ? r : (r & 0x3 | 0x8);
-                return v.toString(16);
-            });
-            return (uuid);
+            return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16));
         },
         /**
          * simple json/csv exporter
