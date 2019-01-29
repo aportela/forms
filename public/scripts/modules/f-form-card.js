c69b8f333fa2e56bb767d27e95453b64d653c35b
alex <766f6964+github@gmail.com>
2019-01-29 20:10:18 +0100

    added lists for attachments, notes & links

--- public/scripts/modules/f-form-card.js
+++ public/scripts/modules/f-form-card.js
@@ -9,15 +9,54 @@ const template = function () {
                         <label class="label">Form title</label>
                         <input class="input" type="text" placeholder="type a descriptive title for this new element">
                     </div>
-                        <div class="tabs">
+                    <div class="tabs">
                         <ul>
-                            <li class="is-active"><a><span class="icon is-small"><i class="fas fa-database" aria-hidden="true"></i></span> Metadata</a></li>
-                            <li v-show="template.allowFormAttachments"><a><span class="icon is-small"><i class="fas fa-paperclip" aria-hidden="true"></i></span> Attachments</a></li>
-                            <li v-show="template.allowFormNotes"><a><span class="icon is-small"><i class="far fa-comment-alt" aria-hidden="true"></i></span> Notes</a></li>
-                            <li v-show="template.allowFormLinks"><a><span class="icon is-small"><i class="fas fa-link" aria-hidden="true"></i></span> Links</a></li>
+                            <li v-bind:class="{ 'is-active': activeTab == 'metadata' }"><a v-on:click.prevent="activeTab = 'metadata'"><span class="icon is-small"><i class="fas fa-database" aria-hidden="true"></i></span> Metadata</a></li>
+                            <li v-bind:class="{ 'is-active': activeTab == 'attachments' }" v-show="template.allowFormAttachments"><a v-on:click.prevent="activeTab = 'attachments'"><span class="icon is-small"><i class="fas fa-paperclip" aria-hidden="true"></i></span> Attachments</a></li>
+                            <li v-bind:class="{ 'is-active': activeTab == 'notes' }" v-show="template.allowFormNotes"><a v-on:click.prevent="activeTab = 'notes'"><span class="icon is-small"><i class="far fa-comment-alt" aria-hidden="true"></i></span> Notes</a></li>
+                            <li v-bind:class="{ 'is-active': activeTab == 'links' }" v-show="template.allowFormLinks"><a v-on:click.prevent="activeTab = 'links'"><span class="icon is-small"><i class="fas fa-link" aria-hidden="true"></i></span> Links</a></li>
                         </ul>
                     </div>
-                    <f-field-form v-for="formField in template.formFields" v-bind:key="formField.id" v-bind:disabled="disabled" v-bind:field="formField" ></f-field-form>
+                    <div v-if="activeTab == 'metadata'">
+                        <f-field-form v-for="formField in template.formFields" v-bind:key="formField.id" v-bind:disabled="disabled" v-bind:field="formField" ></f-field-form>
+                    </div>
+                    <div v-else-if="activeTab == 'attachments'">
+                        <table class="table is-striped is-narrow is-fullwidth">
+                            <thead>
+                                <tr>
+                                    <th>Date</th>
+                                    <th>User</th>
+                                    <th>Filename</th>
+                                    <th>Size</th>
+                                    <th>Operations</th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+                    <div v-else-if="activeTab == 'notes'">
+                        <table class="table is-striped is-narrow is-fullwidth">
+                            <thead>
+                                <tr>
+                                    <th>Date</th>
+                                    <th>User</th>
+                                    <th>Body</th>
+                                    <th>Operations</th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+                    <div v-else-if="activeTab == 'links'">
+                        <table class="table is-striped is-narrow is-fullwidth">
+                            <thead>
+                                <tr>
+                                    <th>Date</th>
+                                    <th>User</th>
+                                    <th>Linked element</th>
+                                    <th>Operations</th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
@@ -27,6 +66,11 @@ const template = function () {
 export default {
     name: 'f-form-card',
     template: template(),
+    data: function () {
+        return ({
+            activeTab: 'metadata'
+        });
+    },
     props: [
         'template',
         'disabled'
