764430fa1d4d7bec7de3a52ffd05c0d60775482c
alex <766f6964+github@gmail.com>
2019-01-30 20:58:51 +0100

    added dummy component for testing upload file progress

--- public/scripts/modules/f-section-container.js
+++ public/scripts/modules/f-section-container.js
@@ -1,3 +1,4 @@
+import { bus } from './bus.js';
 import { default as vueFormsTopMenu } from './f-top-menu.js';
 import { default as vueFormsBreadcrumb } from './f-breadcrumb.js';
 
@@ -7,6 +8,7 @@ const template = function () {
             <f-top-menu></f-top-menu>
             <div class="section">
                 <f-breadcrumb></f-breadcrumb>
+                <progress v-if="uploading" class="progress" v-bind:value="progress" max="100">{{ progress }}%</progress>
                 <router-view></router-view>
             </div>
         </div>
@@ -18,9 +20,27 @@ export default {
     template: template(),
     data: function () {
         return ({
+            uploading: false,
+            progress: 0
         });
-    }, created: function () {
+    },
+    created: function () {
         console.log("[section container]: created");
+        let self = this;
+        bus.$on('uploadFile', function(file) {
+            console.log("received file");
+            self.progress = 0;
+            self.uploading = true;
+            let interval = setInterval(function(){
+                if (self.progress == 100) {
+                    clearInterval(interval);
+                    self.uploading = false;
+                    bus.$emit('fileUploaded', file);
+                } else {
+                    self.progress++;
+                }
+            }, 2);
+        });
     },
     components: {
         'f-top-menu': vueFormsTopMenu,
