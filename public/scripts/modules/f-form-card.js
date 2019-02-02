e49f56000fa4f86e23e68e7c88a1cb005c4aa171
alex <766f6964+github@gmail.com>
2019-02-02 20:52:39 +0100

    changed form class container (box -> card)

--- public/scripts/modules/f-form-card.js
+++ public/scripts/modules/f-form-card.js
@@ -6,132 +6,147 @@ import { default as fieldForm } from './f-field-form.js';
 const template = function () {
     return `
         <div class="columns">
-            <div class="column is-three-fifths is-offset-one-fifth">
-                <div class="box">
-                    <div class="field">
-                        <label class="label">Form title</label>
-                        <input class="input" type="text" placeholder="type a descriptive title for this new element">
-                    </div>
-                    <div class="tabs">
-                        <ul>
-                            <li v-bind:class="{ 'is-active': activeTab == 'metadata' }"><a v-on:click.prevent="activeTab = 'metadata'"><span class="icon is-small"><i class="fas fa-database" aria-hidden="true"></i></span> Metadata</a></li>
-                            <li v-bind:class="{ 'is-active': activeTab == 'attachments' }" v-show="template.allowFormAttachments"><a v-on:click.prevent="activeTab = 'attachments'"><span class="icon is-small"><i class="fas fa-paperclip" aria-hidden="true"></i></span> Attachments ({{ attachments.length }})</a></li>
-                            <li v-bind:class="{ 'is-active': activeTab == 'notes' }" v-show="template.allowFormNotes"><a v-on:click.prevent="activeTab = 'notes'"><span class="icon is-small"><i class="far fa-comment-alt" aria-hidden="true"></i></span> Notes ({{ notes.length }})</a></li>
-                            <li v-bind:class="{ 'is-active': activeTab == 'links' }" v-show="template.allowFormLinks"><a v-on:click.prevent="activeTab = 'links'"><span class="icon is-small"><i class="fas fa-link" aria-hidden="true"></i></span> Links</a></li>
-                        </ul>
-                    </div>
-                    <div v-if="activeTab == 'metadata'">
-                        <f-field-form v-for="formField in template.formFields" v-bind:key="formField.id" v-bind:disabled="disabled" v-bind:field="formField" ></f-field-form>
-                    </div>
-                    <div v-else-if="activeTab == 'attachments'">
-                        <div class="file has-name is-fullwidth">
-                            <label class="file-label">
-                                <input class="file-input" type="file" name="resume" v-on:change="onFileUploadChange" ref="f">
-                                <span class="file-cta">
-                                    <span class="file-icon">
-                                        <i class="fas fa-upload"></i>
-                                    </span>
-                                    <span class="file-label">
-                                        Choose a file…
-                                    </span>
-                                </span>
-                                <span class="file-name">{{ fileName }}</span>
-                            </label>
-                        </div>
+            <div class="column is-10 is-offset-1">
+                <div class="card">
+                    <header class="card-header">
+                        <p class="card-header-title">
+                            Create form with template "foo"
+                        </p>
+                        <a href="#" class="card-header-icon" aria-label="more options">
+                            <button type="button" class="button is-small is-fullwidth is-info" v-bind:disabled="disabled" v-on:click="$router.push({ name: 'updateAttribute', params: { id: attribute.id } })">
+                                <span class="icon is-small"><i class="fas fa-pen"></i></span>
+                                <span>Save</span>
+                            </button>
+                        </a>
+                    </header>
 
-                        <table class="table is-striped is-narrow is-fullwidth">
-                            <thead>
-                                <tr>
-                                    <th>Date</th>
-                                    <th>User</th>
-                                    <th>Filename</th>
-                                    <th>Size</th>
-                                    <th>Comment/description</th>
-                                    <th>Operations</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr v-for="item in attachments" v-bind:key="item.id">
-                                    <td>{{ item.date | parseJSONDateTime }}</td>
-                                    <td>{{ item.creator.name }}</td>
-                                    <td>{{ item.filename }}</td>
-                                    <td>{{ item.fileSize | parseHumanFileSize }}</td>
-                                    <td></td>
-                                    <td>
-                                        <div class="field is-grouped">
-                                            <p class="control is-expanded">
-                                                <button type="button" class="button is-small is-fullwidth is-link" v-bind:disabled="disabled">
-                                                    <span class="icon is-small"><i class="fas fa-download"></i></span>
-                                                    <span>Download</span>
-                                                </button>
-                                            </p>
-                                            <p class="control is-expanded">
-                                                <button type="button" class="button is-small is-fullwidth is-danger" v-bind:disabled="disabled">
-                                                    <span class="icon is-small"><i class="fas fa-trash-alt"></i></span>
-                                                    <span>Remove</span>
-                                                </button>
-                                            </p>
-                                        </div>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-
-
-                    <div v-else-if="activeTab == 'notes'">
+                    <div class="card-content">
 
                         <div class="field">
-                            <label class="label">Message</label>
-                            <div class="control">
-                                <textarea class="textarea" placeholder="type note body" v-model="noteBody"></textarea>
-                            </div>
+                            <label class="label">Form title</label>
+                            <input class="input" type="text" placeholder="type a descriptive title for this new element">
                         </div>
-                        <div class="field is-grouped">
-                            <div class="control">
-                                <button class="button is-link" v-on:click.prevent="addNote()">Add note</button>
+                        <div class="tabs">
+                            <ul>
+                                <li v-bind:class="{ 'is-active': activeTab == 'metadata' }"><a v-on:click.prevent="activeTab = 'metadata'"><span class="icon is-small"><i class="fas fa-database" aria-hidden="true"></i></span> Metadata</a></li>
+                                <li v-bind:class="{ 'is-active': activeTab == 'attachments' }" v-show="template.allowFormAttachments"><a v-on:click.prevent="activeTab = 'attachments'"><span class="icon is-small"><i class="fas fa-paperclip" aria-hidden="true"></i></span> Attachments ({{ attachments.length }})</a></li>
+                                <li v-bind:class="{ 'is-active': activeTab == 'notes' }" v-show="template.allowFormNotes"><a v-on:click.prevent="activeTab = 'notes'"><span class="icon is-small"><i class="far fa-comment-alt" aria-hidden="true"></i></span> Notes ({{ notes.length }})</a></li>
+                                <li v-bind:class="{ 'is-active': activeTab == 'links' }" v-show="template.allowFormLinks"><a v-on:click.prevent="activeTab = 'links'"><span class="icon is-small"><i class="fas fa-link" aria-hidden="true"></i></span> Links</a></li>
+                            </ul>
+                        </div>
+                        <div v-if="activeTab == 'metadata'">
+                            <f-field-form v-for="formField in template.formFields" v-bind:key="formField.id" v-bind:disabled="disabled" v-bind:field="formField" ></f-field-form>
+                        </div>
+                        <div v-else-if="activeTab == 'attachments'">
+                            <div class="file has-name is-fullwidth">
+                                <label class="file-label">
+                                    <input class="file-input" type="file" name="resume" v-on:change="onFileUploadChange" ref="f">
+                                    <span class="file-cta">
+                                        <span class="file-icon">
+                                            <i class="fas fa-upload"></i>
+                                        </span>
+                                        <span class="file-label">
+                                            Choose a file…
+                                        </span>
+                                    </span>
+                                    <span class="file-name">{{ fileName }}</span>
+                                </label>
                             </div>
+
+                            <table class="table is-striped is-narrow is-fullwidth">
+                                <thead>
+                                    <tr>
+                                        <th>Date</th>
+                                        <th>User</th>
+                                        <th>Filename</th>
+                                        <th>Size</th>
+                                        <th>Comment/description</th>
+                                        <th>Operations</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr v-for="item in attachments" v-bind:key="item.id">
+                                        <td>{{ item.date | parseJSONDateTime }}</td>
+                                        <td>{{ item.creator.name }}</td>
+                                        <td>{{ item.filename }}</td>
+                                        <td>{{ item.fileSize | parseHumanFileSize }}</td>
+                                        <td></td>
+                                        <td>
+                                            <div class="field is-grouped">
+                                                <p class="control is-expanded">
+                                                    <button type="button" class="button is-small is-fullwidth is-link" v-bind:disabled="disabled">
+                                                        <span class="icon is-small"><i class="fas fa-download"></i></span>
+                                                        <span>Download</span>
+                                                    </button>
+                                                </p>
+                                                <p class="control is-expanded">
+                                                    <button type="button" class="button is-small is-fullwidth is-danger" v-bind:disabled="disabled">
+                                                        <span class="icon is-small"><i class="fas fa-trash-alt"></i></span>
+                                                        <span>Remove</span>
+                                                    </button>
+                                                </p>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
 
-                        <table class="table is-striped is-narrow is-fullwidth">
-                            <thead>
-                                <tr>
-                                    <th>Date</th>
-                                    <th>User</th>
-                                    <th>Body</th>
-                                    <th>Operations</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr v-for="item in notes" v-bind:key="item.id">
-                                    <td>{{ item.date | parseJSONDateTime }}</td>
-                                    <td>{{ item.creator.name }}</td>
-                                    <td>{{ item.body }}</td>
-                                    <td>
-                                        <div class="field is-grouped">
-                                            <p class="control is-expanded">
-                                                <button type="button" class="button is-small is-fullwidth is-danger" v-bind:disabled="disabled">
-                                                    <span class="icon is-small"><i class="fas fa-trash-alt"></i></span>
-                                                    <span>Remove</span>
-                                                </button>
-                                            </p>
-                                        </div>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <div v-else-if="activeTab == 'links'">
-                        <table class="table is-striped is-narrow is-fullwidth">
-                            <thead>
-                                <tr>
-                                    <th>Date</th>
-                                    <th>User</th>
-                                    <th>Linked element</th>
-                                    <th>Operations</th>
-                                </tr>
-                            </thead>
-                        </table>
+
+                        <div v-else-if="activeTab == 'notes'">
+
+                            <div class="field">
+                                <label class="label">Message</label>
+                                <div class="control">
+                                    <textarea class="textarea" placeholder="type note body" v-model="noteBody"></textarea>
+                                </div>
+                            </div>
+                            <div class="field is-grouped">
+                                <div class="control">
+                                    <button class="button is-link" v-on:click.prevent="addNote()">Add note</button>
+                                </div>
+                            </div>
+
+                            <table class="table is-striped is-narrow is-fullwidth">
+                                <thead>
+                                    <tr>
+                                        <th>Date</th>
+                                        <th>User</th>
+                                        <th>Body</th>
+                                        <th>Operations</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr v-for="item in notes" v-bind:key="item.id">
+                                        <td>{{ item.date | parseJSONDateTime }}</td>
+                                        <td>{{ item.creator.name }}</td>
+                                        <td>{{ item.body }}</td>
+                                        <td>
+                                            <div class="field is-grouped">
+                                                <p class="control is-expanded">
+                                                    <button type="button" class="button is-small is-fullwidth is-danger" v-bind:disabled="disabled">
+                                                        <span class="icon is-small"><i class="fas fa-trash-alt"></i></span>
+                                                        <span>Remove</span>
+                                                    </button>
+                                                </p>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div v-else-if="activeTab == 'links'">
+                            <table class="table is-striped is-narrow is-fullwidth">
+                                <thead>
+                                    <tr>
+                                        <th>Date</th>
+                                        <th>User</th>
+                                        <th>Linked element</th>
+                                        <th>Operations</th>
+                                    </tr>
+                                </thead>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
